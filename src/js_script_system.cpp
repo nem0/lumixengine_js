7b81d975df162e93721469212d512a29baf0aaae
Mikulas Florek <mikulas.florek@gamedev.sk>
2023-04-06 01:10:53 +0200

    synced with engine

--- src/js_script_system.cpp
+++ src/js_script_system.cpp
@@ -983,12 +983,11 @@ public:
 	}
 
 
-	void update(float time_delta, bool paused) override {
+	void update(float time_delta) override {
 		PROFILE_FUNCTION();
 
-		if (m_is_game_running && !m_scripts_init_called) initScripts();
-
-		if (paused || !m_is_game_running) return;
+		if (!m_is_game_running) return;
+		if (!m_scripts_init_called) initScripts();
 
 		for (int i = 0; i < m_updates.size(); ++i) {
 			UpdateData update_item = m_updates[i];
